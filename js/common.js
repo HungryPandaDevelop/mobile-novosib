0fbb9d27255d5d5ce382b347838c4441c14c1624
HungryPanda <bink@inbox.ru>
2020-03-09 12:16:35 +0300

    save-2

--- js/common.js
+++ js/common.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
+  console.log("test");
+  var countInput = 0;
+  var goodsMass = ["code", "name", "count"];
+  $(".cell-content").on("click", ".add-goods", function (e) {
+    e.preventDefault();
+    var cloneObj = $(this).parents(".cell-inner").clone();
+    countInput++;
 
+    for (var i = 0; i < goodsMass.length; i++) {
+      cloneObj.find(".goods-" + goodsMass[i]).attr("name", "goods-" + goodsMass[i] + "-" + countInput);
+    }
+    $(this).parents(".cell-inner").after(cloneObj);
+    $(this).remove();
+  });
+  var specialCheckbox = $(".special-checkbox .sc-item");
+  specialCheckbox.on("click", function () {
+    specialCheckbox.removeClass("active").eq($(this).index()).addClass("active");
+  });
 });
